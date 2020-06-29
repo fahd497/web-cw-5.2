0026bac66a2ac7d09fe7d294e58bee9ff1c34e60
fahdhadhoud <66736069+fahdhadhoud@users.noreply.github.com>
2020-06-29 21:47:46 +0300

    خلصت تمام

---
+++ Development/js/scripts.js
@@ -0,0 +1,27 @@
+let food =prompt(" ماذا تحب ان تطلب");
+if (food==1) {
+    console.log( "لحم" );
+}
+else if (food==2) {
+     console.log ( "دجاج" );
+}
+else if (food==3) {  
+    console.log ( "سمك" );
+} else {
+    console.log ( " لايوجد" );
+
+}
+let ht=prompt("ادخل الرقم الاول")
+
+  let hr=prompt("ادخل الرقم الثاني")
+  alert(ht*hr)
+
+
+  let age=prompt(" اكتب عمرك لكي تعلم انت ب الجامعة او مدرسة")
+
+  if (age >= 19) {
+   alert ( "جامعة")
+  }
+   else {
+   alert( "مدرسة")
+  }
